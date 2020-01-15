4c5aa4cecb7e6dad211a8dea84df1aa10bb87e2c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-15 13:16:07 +0100

    cambiamenti

--- Negozio_Magazzino/MainWindow.xaml.cs
+++ Negozio_Magazzino/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ using System.Windows.Shapes;
 namespace Negozio_Magazzino
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml 
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -24,5 +25,17 @@ namespace Negozio_Magazzino
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string prodotto = txtProdotto.Text;
+            double costo = double.Parse(txtPrezzo.Text);
+            lblRisultato.Content += $"PRODOTTO: {prodotto}, PREZZO: {costo}\n";
+            StreamWriter sw = new StreamWriter("pubblica.Text", true);
+            {
+                sw.WriteLine($"PRODOTTO: {prodotto}, PREZZO: {costo} \n");
+            }
+        }
     }
 }
+
